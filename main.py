be30b6c14091f46b061e5648ef224b818601e077
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-09-29 12:15:09 +0100

    Add default values for some config settings

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from promo import Course
 CONSUMER_KEY = config('TWITTER_API_KEY')
 CONSUMER_SECRET = config('TWITTER_SECRET_KEY')
 TWITTER_TARGET_ACCOUNT = config('TWITTER_TARGET_ACCOUNT')
-LAST_MANAGED_TWEET_FILE = config('LAST_MANAGED_TWEET_FILE')
-API_WINDOW_SIZE = config('API_WINDOW_SIZE', cast=int)
-SEARCH_TERMS_FILE = config('SEARCH_TERMS_FILE')
+LAST_MANAGED_TWEET_FILE = config(
+    'LAST_MANAGED_TWEET_FILE', default='last-managed-tweet.dat'
+)
+API_WINDOW_SIZE = config('API_WINDOW_SIZE', cast=int, default=10)
+SEARCH_TERMS_FILE = config('SEARCH_TERMS_FILE', default='search-terms.dat')
 SLACK_API_TOKEN = config('SLACK_API_TOKEN')
 SLACK_CHANNEL = config('SLACK_CHANNEL')
 
