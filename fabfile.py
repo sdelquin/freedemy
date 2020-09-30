c7339b3b356d31e669a51af3efd193c5f0c7041e
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-09-30 09:18:59 +0100

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import local, cd, run, env, prefix
+
+PROJECT = 'freedemy'
+
+env.hosts = ['cloud']
+
+
+def deploy():
+    local('git push')
+    with prefix(f'source ~/.virtualenvs/{PROJECT}/bin/activate'):
+        with cd(f'~/code/{PROJECT}'):
+            run('git pull')
+            run('pip install -r requirements.txt')
