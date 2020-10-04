95a48aff6254cbd64cea667cdf183d0d9eb6c450
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-10-04 14:15:29 +0100

    Fix fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,17 @@
-from pathlib import Path
+import pathlib
 
 from fabric.api import cd, env, local, prefix, run
 
-PROJECT = Path('.').parent.absolute().parts[-1]
+PROJECT = pathlib.Path('.').parent.absolute().parts[-1]
 env.hosts = ['cloud']
 
 
 def deploy():
+    '''Deploy application to remote server'''
+
     local('git push')
     with prefix(f'source ~/.virtualenvs/{PROJECT}/bin/activate'):
         with cd(f'~/code/{PROJECT}'):
             run('git pull')
             run('pip install -r requirements.txt')
-            run(f'supervisortctl restart rq_{PROJECT}')
+            run(f'supervisorctl restart rq_{PROJECT}')
