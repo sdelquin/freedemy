4bb651a5d68add4f8e738e298ea671359e0ec614
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-04-28 12:57:46 +0100

    Fix tests

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+
+from freedemy import settings
+from freedemy.delivery import SlackDelivery
+from freedemy.promo import Course
+
+# Spanish Free Course
+FREE_COURSE_URL = 'https://comidoc.net/udemy/fundamentos-de-big-data-en-aws'
+# English Couponed Course
+COUPONED_COURSE_URL = (
+    'https://comidoc.net/udemy/nginx-apache-ssl-encryption-certification-course'
+)
+
+
+@pytest.fixture
+def delivery_service():
+    return SlackDelivery(settings.SLACK_API_TOKEN, settings.SLACK_CHANNEL)
+
+
+@pytest.fixture
+def free_course():
+    return Course(FREE_COURSE_URL)
+
+
+@pytest.fixture
+def couponed_course():
+    return Course(COUPONED_COURSE_URL)
--- tests/test_delivery.py
+++ tests/test_delivery.py
@@ -1,22 +1,2 @@
-import pytest
-from prettyconf import config
-
-from freedemy import settings
-from freedemy.delivery import SlackDelivery
-from freedemy.promo import Course
-
-
-@pytest.fixture
-def delivery_service():
-    return SlackDelivery(settings.SLACK_API_TOKEN, settings.SLACK_CHANNEL)
-
-
-@pytest.fixture
-def course():
-    c = Course(config('COURSE_TRACKER_TEST_URL'), settings.UDEMY_COURSES_BASE_URL)
-    c.get_course_tracker_data()
-    return c
-
-
-def test_post(delivery_service, course):
-    assert delivery_service.post(course) is not None
+def test_post(delivery_service, couponed_course):
+    assert delivery_service.post(couponed_course) is not None
