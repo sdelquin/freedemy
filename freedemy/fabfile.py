b28db7c98e911a41b188aa3816eb4b612daf9c72
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-04-28 14:20:21 +0100

    Fix fabfile

--- freedemy/fabfile.py
+++ freedemy/fabfile.py
@@ -1,16 +1,16 @@
-import pathlib
-
 from fabric.api import cd, env, local, prefix, run
 
-PROJECT = pathlib.Path('.').parent.absolute().parts[-1]
+from settings import PROJECT_DIR
+
 env.hosts = ['cloud']
+PROJECT_NAME = PROJECT_DIR.stem
 
 
 def deploy():
     '''Deploy application to remote server'''
 
     local('git push')
-    with prefix(f'source ~/.virtualenvs/{PROJECT}/bin/activate'):
-        with cd(f'~/code/{PROJECT}'):
+    with prefix(f'source ~/.virtualenvs/{PROJECT_DIR.stem}/bin/activate'):
+        with cd(f'~/code/{PROJECT_NAME}'):
             run('git pull')
             run('pip install -r requirements.txt')
