7d4ac6bb0c94481e0a5d8690b7e41ecbaabf3619
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-11-22 12:22:43 +0000

    Use css selector for coupon button

--- freedemy/promo.py
+++ freedemy/promo.py
@@ -55,11 +55,15 @@ class Course:
         logger.info('Getting html contents...')
 
         self.webdriver.get(self.course_tracker_url)
-        coupon_button_xpath = '//*[@id="__next"]/div/div[2]/div/div[5]/div[2]/div/button'
+        coupon_button_css_selector = (
+            '#__next > div > div.pt-14.bg-th-background > div > '
+            r'div.mx-auto.items-center.block.space-y-4.md\:space-y-0.p-8.md\:'
+            'flex.justify-evenly.bg-th-background.max-w-5xl.pt-8.pb-8 > div > div > button'
+        )
         element = WebDriverWait(self.webdriver, 10).until(
             # button with link to Udemy ↓
             # SHARE ON FACEBOOK | 100%OFF COUPON | SHARE ON TWITTER
-            EC.presence_of_element_located((By.XPATH, coupon_button_xpath))
+            EC.presence_of_element_located((By.CSS_SELECTOR, coupon_button_css_selector))
         )
         if '100%OFF' in element.text:
             self.is_couponed = True
